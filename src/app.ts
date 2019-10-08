3168f31afab2bcd5e42e2374be0fc373f2efe3b3
parkchanjong <7000cj@gmail.com>
2019-10-08 19:58:56 +0900

    apollo setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import { ApolloServer, gql } from 'apollo-server-koa';
 import routes from './routes';
 
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(routes.routes()).use(routes.allowedMethods());
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'hi'
+  }
+};
+
+const apollo = new ApolloServer({ typeDefs, resolvers });
+apollo.applyMiddleware({ app });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+import './env';
 import app from './app';
 
-app.listen(4000, () => {
-  console.log('fight server is listening to port');
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log('Fight server is listening to port', PORT);
 });
